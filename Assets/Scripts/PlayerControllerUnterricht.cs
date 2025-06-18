96999362f39ec1d9b346470e487eeb6decc5bd6c
N1ghtfoxx <2024.gme.naomizel@gmail.com>
2025-06-18 11:28:10 +0200

    UPD PlayerControllerUnterricht played with Terrain in Unterricht-Scene

--- Assets/Scripts/PlayerControllerUnterricht.cs
+++ Assets/Scripts/PlayerControllerUnterricht.cs
@@ -9,6 +9,9 @@ public class PlayerControllerUnterricht : MonoBehaviour
     private Vector2 movementInput;
     private Transform cameraTransform;
     [SerializeField] private float actualMovementSpeed = 5f;
+    [SerializeField] private float jumpStrength;
+    private Rigidbody rb;
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,13 +21,23 @@ public class PlayerControllerUnterricht : MonoBehaviour
         playerInput.actions["Move"].performed += OnMove;
         playerInput.actions["Move"].canceled += OnMove;
 
+        playerInput.actions["Jump"].performed += OnJump;
+
         cameraTransform = Camera.main.transform;
-        //cameraTransform.forward = transform.forward; // Ensure the camera's forward direction is aligned with the player's forward direction
+
+        rb = GetComponent<Rigidbody>();
     }
 
     public void OnMove(CallbackContext ctx)
     {
         movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnJump(CallbackContext ctx)
+    {
+        if (!isGrounded) return; // Only allow jumping if the player is grounded
+        rb.AddForce(Vector3.up * jumpStrength); // Add an upward force for jumping
+        Debug.Log("Jump");
     }
 
     // Update is called once per frame
@@ -34,5 +47,15 @@ public class PlayerControllerUnterricht : MonoBehaviour
         movementDirection = Vector3.ProjectOnPlane(movementDirection, Vector3.up).normalized; // Project movement direction onto the horizontal plane
 
         transform.Translate(movementDirection * actualMovementSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true; // Set isGrounded to true when colliding with the ground
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false; // Set isGrounded to false when no longer colliding with the ground
     }
 }
